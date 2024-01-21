9f711f705038c8e865ab9159f52eb4bba06c6359
EduardoProfe <eduardoglez64377@gmail.com>
2024-01-21 17:15:47 -0500

    Update script.py

--- script.py
+++ script.py
@@ -20,6 +20,11 @@ contents = repo.get_contents(file_path)
 file_content = \
     """
 # World Greetings
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+![GitHub Actions](https://img.shields.io/badge/github%20actions-%232671E5.svg?style=for-the-badge&logo=githubactions&logoColor=white)
+
 This is basically a `python bot` that generates greetings every 15 minutes to
 every part of the world.
 
