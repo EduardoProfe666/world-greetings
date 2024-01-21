91975535c24165af447a85fce806b0f8f7183683
EduardoProfe <eduardoglez64377@gmail.com>
2024-01-21 15:15:53 -0500

    Update script.py

--- script.py
+++ script.py
@@ -16,6 +16,21 @@ g = Github(github_token)
 repo = g.get_repo(repo_name)
 
 for tz in all_timezones:
-    if "Cuba" in tz:
-        print(datetime.now(timezone(tz)).strftime('%A %d/%B/%Y %I:%M %p'))
-        break
+    t = datetime.now(timezone(tz))
+    day = t.strftime('%I:%M %p')
+    time = t.strftime('%I:%M %p')
+    gr = "Good Evening"
+    if 1 <= t.hour < 12:
+        gr = "Good Morning"
+    elif 12 <= t.hour < 18:
+        gr = "Good Afternoon"
+
+    filename = f"greetings/{tz}.md"
+    file_content = \
+        f"""## 👋 {gr} `{tz}`
+    ### 📅 Today is `{day}`
+    ### ⌚ It's `{time}` there
+    ### 🎩 With love, EduardoProfe666 
+    """
+    repo.
+    repo.create_file(filename, "Add .md file for timezone", file_content)
